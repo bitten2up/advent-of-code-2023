23be5d86e38948c0e536752972b2d4747d3ef461
bitten2up <bitten2up@courvix.com>
2023-12-08 21:20:06 +0000

    e

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,34 +1,57 @@
-use std::fs;
-use std::fs::{File, OpenOptions};
-use std::io;
-use std::io::prelude::*;
-use std::path::Path;
-use std::env;
+use std::fs::File;
 use std::io::Read;
 
 // holy shit the joke program became useful
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    /*let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         error_operation();
         return;
-    }
-    parse_challenge(args);
+    }*/
+    parse_challenge();
     return;
 }
-fn parse_challenge(args: Vec<String>) {
+fn parse_challenge() {
     let mut file = File::open("test.txt").expect("error: file not found");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("can't read for some reason...");
     println!("{}", contents);
-    for i in 0..contents.length()
+    let mut games:i32 = 0;
+    let mut status:i32 = 0; // 0 for dont parse yet
+    let mut counter:i32 = 0;
+    for c in contents.chars()
     {
-        
+        if (c <= '0' && c >= '9')
+        {
+            counter+=1;
+            println!("{}", counter);
+        }
+        // see if first char for parsing is found
+        if c == ':'
+        {
+            status = 1;
+            counter = 0;
+        }
+        // see if second control char is found
+        if c == ';'
+        {
+            status = 3;
+            counter = 0;
+        }
+        if c == ' '
+        {
+            counter = 0;
+        }
+        if c == '\n'
+        {
+            println!("{games}");
+            games += 1;
+            status = 0;
+        }
+        if status == 1
+        {
+            counter += 1;
+        }
     }
-}
-
-fn error_operation() {
-    println!("error: does not contain file");
-    println!("example of how to run command: day-2 <file.txt>");
 }
